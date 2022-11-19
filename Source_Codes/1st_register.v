869bd93dbf395cfd01de4870091f5d93f0285840
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-19 23:32:10 +0500

    Formated the code for 1st_register.v to look more beautiful

--- Source_Codes/1st_register.v
+++ Source_Codes/1st_register.v
@@ -1,28 +1,36 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ps
+
 module first_register (
-    input clk,rst,StallD,FlushD,
-    input [31:0] instruction,PCF,PCPlus4F,
-    output reg [31:0] instrD, PCD,PCPlus4D
+    input             clk,
+    input             rst,
+    input             StallD,
+    input             FlushD,
+    input      [31:0] instruction,
+    input      [31:0] PCF,
+    input      [31:0] PCPlus4F,
+    output reg [31:0] instrD, 
+    output reg [31:0] PCD,
+    output reg [31:0] PCPlus4D
 );
     always @(posedge clk) begin
         if (rst) begin
-           instrD <= 32'd0;
-            PCD <= 32'd0;
+           instrD    <= 32'd0;
+            PCD      <= 32'd0;
             PCPlus4D <= 32'd0;
         end
         else if (StallD) begin
-            instrD <= instrD;
-            PCD <= PCD;
+            instrD   <= instrD;
+            PCD      <= PCD;
             PCPlus4D <= PCPlus4D;
         end
         else if (FlushD) begin
-            instrD <= 32'd0;
-            PCD <= 32'd0;
+            instrD   <= 32'd0;
+            PCD      <= 32'd0;
             PCPlus4D <= 32'd0;
         end
         else begin
-            instrD <= instruction;
-            PCD <= PCF;
+            instrD   <= instruction;
+            PCD      <= PCF;
             PCPlus4D <= PCPlus4F;
         end
         
