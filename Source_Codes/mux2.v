6ada28f57a0f9ca3ecd9629bb4af9a7537f75e1f
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-20 00:20:28 +0500

    format mux2.v

--- Source_Codes/mux2.v
+++ Source_Codes/mux2.v
@@ -1,10 +1,12 @@
-module mux2_alu #(parameter WIDTH = 8) 
-                    (input [WIDTH-1:0] WriteDataE, ImmExtE,
-                       input              ALUSrcE,
-                       output [WIDTH-1:0] SrcBE);
-     assign SrcBE = ALUSrcE ?  ImmExtE:WriteDataE;
+module mux2_alu #
+(
+   parameter WIDTH = 8
+) (
+   input [WIDTH-1:0]  WriteDataE,
+   input [WIDTH-1:0]  ImmExtE,
+   input              ALUSrcE,
+   output [WIDTH-1:0] SrcBE
+);
+   assign SrcBE = ALUSrcE ?  ImmExtE:WriteDataE;
+
 endmodule
-
-
-// To call this module:
-//  mux2_alu #(32)   mux(RegisterData, ImmValue, ContSelector, OutB);
