aabf2a71919e40d35ffb0d619567cc885b0a6271
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-19 23:41:40 +0500

    format the code for third_register.v

--- Source_Codes/3rd_register.v
+++ Source_Codes/3rd_register.v
@@ -1,32 +1,41 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ps
+
 module third_register (
-    input [31:0] WriteDataE,ALUResult,PCPlus4E,
-    input [4:0] RdE,
-    input clk,rst,RegWriteE,MemWriteE,
-    input [1:0] ResultSrcE,
-    output reg [31:0] ALUResultM, WriteDataM, PCPlus4M,
-    output reg [4:0] RdM,
-    output reg RegWriteM,MemWriteM,
-    output reg [1:0] ResultSrcM
+    input      [31:0] WriteDataE,
+    input      [31:0] ALUResult,
+    input      [31:0] PCPlus4E,
+    input      [4:0]  RdE,
+    input             clk,
+    input             rst,
+    input             RegWriteE,
+    input             MemWriteE,
+    input      [1:0]  ResultSrcE,
+    output reg [31:0] ALUResultM,
+    output reg [31:0] WriteDataM,
+    output reg [31:0] PCPlus4M,
+    output reg [4:0]  RdM,
+    output reg        RegWriteM,
+    output reg        MemWriteM,
+    output reg [1:0]  ResultSrcM
 );
     always @(posedge clk) begin
         if (rst) begin
-           RegWriteM <= 0;
-           MemWriteM <= 0;
+           RegWriteM  <= 0;
+           MemWriteM  <= 0;
            ResultSrcM <= 2'b00;
            ALUResultM <= 32'd0;
            WriteDataM <= 32'd0;
-           RdM <= 5'd0;
-           PCPlus4M <= 32'd0;
+           RdM        <= 5'd0;
+           PCPlus4M   <= 32'd0;
         end
         else begin
-           RegWriteM <= RegWriteE;
-           MemWriteM <= MemWriteE;
+           RegWriteM  <= RegWriteE;
+           MemWriteM  <= MemWriteE;
            ResultSrcM <= ResultSrcE;
            ALUResultM <= ALUResult;
            WriteDataM <= WriteDataE;
-           RdM <= RdE;
-           PCPlus4M <= PCPlus4E;
+           RdM        <= RdE;
+           PCPlus4M   <= PCPlus4E;
         end
     end
     
