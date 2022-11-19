e6d18f7a9844752892a79a97b381ce590a57ce4b
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-19 23:55:43 +0500

    format the code for alu

--- Source_Codes/alu.v
+++ Source_Codes/alu.v
@@ -35,72 +35,58 @@
 | 1111  |   ALU_Out = 1 if A=B else 0;
 ----------------------------------------------------------------------*/
 module alu(
-           input [31:0] SrcAE,SrcBE,  // ALU 32-bit Inputs
-           input [4:0] ALUControlE,// ALU Selection
-           input [2:0] funct3E,
-           output[31:0] ALUResult, // ALU 32-bit Output
-           output CarryOut, // Carry Out Flag
-		   output reg ZeroE	// Zero Flag
-    );
-    reg [31:0] ALU_Result;
-    wire [32:0] tmp;
-    assign ALUResult = ALU_Result; // ALU out
-    assign tmp = {1'b0,SrcAE} + {1'b0,SrcBE};
-    assign CarryOut = tmp[32]; // Carryout flag
-	//assign Zero = (ALU_Result == 0);  // Zero Flag ALU_Result
-	always @(*)
+
+    input      [31:0] SrcAE,SrcBE,  // ALU 32-bit Inputs
+    input      [4:0]  ALUControlE,  // ALU Selection
+    input      [2:0]  funct3E,
+    output reg [31:0] ALUResult,    // ALU 32-bit Output
+    output reg        CarryOut,     // Carry Out Flag
+    output reg        ZeroE         // Zero Flag
+
+);
+
+    reg  [31:0]  ALU_Result;
+    wire [32:0]  tmp;
+
+    assign ALUResult = ALU_Result;              // ALU out
+    assign tmp       = {1'b0, SrcAE} + {1'b0, SrcBE};
+    assign CarryOut  = tmp[32];                  // Carryout flag
+
+	always @ (*) //combinational block so blocking (=) assignment should be used
 	begin
 	   case(funct3E)
-	   3'b000: ZeroE <= SrcAE == SrcBE; //beq
-	   3'b001: ZeroE <=  SrcAE != SrcBE; //bne
-	   3'b100: ZeroE <=  SrcAE < SrcBE; //blt
-	   3'b101: ZeroE <=  SrcAE > SrcBE; //bge
-	   3'b110: ZeroE <=  SrcAE < SrcBE; //bltu
-	   3'b111: ZeroE <=  SrcAE >= SrcBE; //bgeu
-	   default : ZeroE <= 0;
+	   3'b000:   ZeroE =  SrcAE == SrcBE; //beq
+	   3'b001:   ZeroE =  SrcAE != SrcBE; //bne
+	   3'b100:   ZeroE =  SrcAE < SrcBE;  //blt
+	   3'b101:   ZeroE =  SrcAE > SrcBE;  //bge
+	   3'b110:   ZeroE =  SrcAE < SrcBE;  //bltu
+	   3'b111:   ZeroE =  SrcAE >= SrcBE; //bgeu
+	   default : ZeroE =  0;
 	   endcase
 	end
-    always @(*)
+
+    always @ (*)
     begin
         case(ALUControlE)
-        5'b00000: ALU_Result = SrcAE + SrcBE ;
-        5'b00001:  ALU_Result = SrcAE - SrcBE ;
-        5'b00010: // Multiplication
-           ALU_Result = SrcAE * SrcBE;
-        5'b00011: // Division
-           ALU_Result = SrcAE/SrcBE;
-        5'b00100: // Logical shift left
-           ALU_Result = SrcAE<<SrcBE;
-         5'b00101: // Logical shift right
-           ALU_Result = SrcAE>>SrcBE;
-         5'b00110: // Rotate left
-           ALU_Result = {SrcAE[30:0],SrcAE[31]};
-         5'b00111: // Rotate right
-           ALU_Result = {SrcAE[0],SrcAE[31:1]};
-          5'b01000: //  Logical and
-           ALU_Result = SrcAE & SrcBE;
-          5'b01001: //  Logical or
-           ALU_Result = SrcAE | SrcBE;
-          5'b01010: //  Logical xor
-           ALU_Result = SrcAE ^ SrcBE;
-          5'b01011: //  Logical nor
-           ALU_Result = ~(SrcAE | SrcBE);
-          5'b01100: // Logical nand
-           ALU_Result = ~(SrcAE & SrcBE);
-          5'b01101: // Logical xnor
-           ALU_Result = ~(SrcAE ^ SrcBE);
-          5'b01110: // Greater comparison
-           ALU_Result = (SrcAE>SrcBE)?32'd1:32'd0 ;
-          5'b01111: // Equal comparison
-            ALU_Result = (SrcAE==SrcBE)?32'd1:32'd0 ;
-          5'b10000:
-            ALU_Result =   SrcBE;
-          default: ALU_Result = SrcAE + SrcBE ;
+        5'b00000: ALU_Result =   SrcAE + SrcBE ;
+        5'b00001: ALU_Result =   SrcAE - SrcBE ;
+        5'b00010: ALU_Result =   SrcAE * SrcBE;
+        5'b00011: ALU_Result =   SrcAE / SrcBE;
+        5'b00100: ALU_Result =   SrcAE<< SrcBE;
+        5'b00101: ALU_Result =   SrcAE>> SrcBE;
+        5'b00110: ALU_Result =  {SrcAE[30:0],SrcAE[31]};
+        5'b00111: ALU_Result =  {SrcAE[0],SrcAE[31:1]};
+        5'b01000: ALU_Result =   SrcAE & SrcBE;
+        5'b01001: ALU_Result =   SrcAE | SrcBE;
+        5'b01010: ALU_Result =   SrcAE ^ SrcBE;
+        5'b01011: ALU_Result = ~(SrcAE | SrcBE);
+        5'b01100: ALU_Result = ~(SrcAE & SrcBE);
+        5'b01101: ALU_Result = ~(SrcAE ^ SrcBE);
+        5'b01110: ALU_Result =  (SrcAE > SrcBE)  ? 32'd1 : 32'd0 ;
+        5'b01111: ALU_Result =  (SrcAE == SrcBE) ? 32'd1 : 32'd0 ;
+        5'b10000: ALU_Result =   SrcBE;
+        default:  ALU_Result =   SrcAE + SrcBE ;
         endcase
     end
-  
-   
-    //always @(*) begin
-      //  Zero <= (ALUResult == 0);
-    //end
+    
 endmodule
