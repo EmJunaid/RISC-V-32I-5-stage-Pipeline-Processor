f7ef2517da36f2107c0f74c241bfbb9c8691f59c
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-20 00:22:37 +0500

    format PCTarget.v

--- Source_Codes/PCTarget.v
+++ Source_Codes/PCTarget.v
@@ -1,7 +1,9 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ps
+
 module PCTarget (
-    input [31:0] PCE,ImmExtE,
-    output [31:0] PCTargetE
+    input      [31:0] PCE,
+    input      [31:0] ImmExtE,
+    output reg [31:0] PCTargetE
 );
     assign PCTargetE = PCE + ImmExtE;
 
