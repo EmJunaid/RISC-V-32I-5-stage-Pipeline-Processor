f86fb3aa18c4e4d98d75a836c3b8f7ef01cd6dd5
Ahsan <ahsan.uet.ee.115@gmail.com>
2022-11-19 23:47:57 +0500

    formated code for Adress_Generator.v and removed non-block assignment from combinational always block

--- Source_Codes/Adress_Generator.v
+++ Source_Codes/Adress_Generator.v
@@ -1,22 +1,33 @@
-`timescale 1ns/1ns
+`timescale 1ns/1ps
+
 module Adress_Generator (
-     input rst,clk,PCSrcE,StallF,
-     //input PCFbar,
-     //input [31:0] ImmExt,
-     input [31:0] PCPlus4F,PCTargetE,
+    input             rst,
+    input             clk,
+    input             PCSrcE,
+    input             StallF,
+
+    input      [31:0] PCPlus4F,
+    input      [31:0] PCTargetE,
     output reg [31:0] PCF
 );
-    reg[31:0] PCFbar;
-    always @(*) begin
-            PCFbar <= PCSrcE?  PCTargetE : PCF + 4;
-        end
-    always @(posedge clk) begin
-        if (rst)
+    reg [31:0] PCFbar;
+
+    always @ (*) begin  //combinational block so block assignment (=) and not a non-blocking one (<=)
+            PCFbar = PCSrcE ? PCTargetE : PCF + 4;
+    end
+
+    always @(posedge clk) begin //sequential block so non-block assigment (<=) is used
+        if (rst) begin
             PCF <= 32'd0;
-        else if(StallF)
+        end
+
+        else if(StallF) begin
              PCF <= PCF;   
-        else
-            PCF <= PCFbar;    
+        end
+
+        else begin
+            PCF <= PCFbar;
+        end
     end 
     
 
